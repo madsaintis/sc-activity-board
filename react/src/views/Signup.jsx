254938251685cb4bc2937e84bd3931823c3a1ab1
madsaintis <muhammadafifazhan@graduate.utm.my>
2023-04-30 14:38:26 +0800

    Added Signup Page

--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Signup() {
+
+  const onSubmit = (event) => {
+    event.preventDefault()
+  }
+
   return (
-    <div>
-        Signup
+    <div className='login-signup-form animated fadeInDown'>
+      <div className='form'>
+        <form onSubmit={onSubmit}>
+
+          <h1 className='title'>
+            Signup 
+          </h1>
+
+          <input type ="name" placeholder='Name' />
+          <input type ="email" placeholder='Email' />
+          <input type ="password" placeholder='Password' />
+          <input type ="password-confirmation" placeholder='Password Confirmation' />
+          <button className='btn btn-block'>Signup</button>
+
+          <p className='message'>
+            Already registered? <Link to="/login">Login now</Link>
+          </p>
+        </form>
+      </div>
+      
     </div>
   )
 }
